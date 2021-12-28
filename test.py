d2cafb6dea288e808756921c576f51b7924d98da
Duvans <duvan.jayasan@gmail.com>
2021-12-28 15:17:01 +0700

    update test

--- test.py
+++ test.py
@@ -6,26 +6,26 @@ import director, movie
     
 
 class TestDirector(unittest.TestCase):
-    def test_read_all_director(self):
+    def testIfDirectorReturnList(self):
         '''test read all director function is return should be list type'''
         self.assertIs(type(director.read_all()),list)
     
 
 class TestMovie(unittest.TestCase):
-    def test_read_all_movie(self):
+    def testIfMovieReturnList(self):
         '''test read all movie function is return should be list type'''
         self.assertIs(type(movie.read_all()),list)
 
 
 class TestApi(unittest.TestCase):
-    def test_get_all_director(self):
+    def testGetDirectors(self):
         '''Test get all data director'''
         client = app.connex_app.app.test_client()
         result = client.get('/api/director')
 
         self.assertEqual(result.status_code, 200)
 
-    def test_get_all_director_with_pagination(self):
+    def testDirectorsWithPagination(self):
         '''
             test Get all director with pagination current page = 80 and data per page = 25
             api : /api/director/{page}/{per_page}
@@ -35,7 +35,7 @@ class TestApi(unittest.TestCase):
         # Make your assertions
         self.assertEqual(result.status_code, 200,'page in director/page/per_page must not exceed max page or data in database')
 
-    def test_get_all_movie_with_pagination(self):
+    def testMoviesWithPagination(self):
         '''
             test Get all movie with pagination current page = 80 and data per page = 25
             api : /api/movie/{page}/{per_page}
@@ -45,14 +45,14 @@ class TestApi(unittest.TestCase):
         # Make your assertions
         self.assertEqual(result.status_code, 200,'current page must not exceed max page or data in database')
 
-    def test_get_all_movie(self):
+    def testGetMovies(self):
         '''Test Get All data Movie'''
         client = app.connex_app.app.test_client()
         result = client.get('/api/movie')
         # Make your assertions
         self.assertEqual(result.status_code, 200) 
 
-    def test_get_movie_by_popularity(self):
+    def testGetMoviesByPopularity(self):
         '''Test Get data Movie sorted by popularity'''
         client = app.connex_app.app.test_client()
         result = client.get('/api/movie/popularity')
